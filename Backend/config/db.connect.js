3e2def0734efc713ff63c8d1e3f27daaca7bbca0
riyashri07 <riyampct7@gmail.com>
2023-06-24 13:51:50 +0530

    connection done

---
+++ Backend/config/db.connect.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+ require("dotenv").config();
+const ConnectDB = async() => {
+try {
+        await mongoose.connect(process.env.mongoUrl);
+        console.log("Connected to DB");
+    } catch (error) {
+        console.log("Cant connect to DB: " + error);
+    }
+};
+
+module.exports = ConnectDB;
+
+
+
+
+// const mongoose = require("mongoose");
+
+// require("dotenv").config();
+
+// const connection = async () => {
+//     try {
+//         await mongoose.connect(process.env.mongoUrl);
+//         console.log("Connected to DB");
+//     } catch (error) {
+//         console.log("Cant connect to DB: " + error);
+//     }
+// };
+// module.exports = connection;
